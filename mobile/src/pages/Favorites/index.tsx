bb49597b818d4a0ea17bfdf3f8199e1487f8c181
W. Guilherme <ravok123@gmail.com>
2020-08-10 18:23:02 -0300

    [Update] Mobile React Native

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './style'
+
+
+function Favorites() {
+   return <View style={styles.container} />
+}
+
+export default Favorites;
